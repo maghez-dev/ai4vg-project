580b3fff6fe0af2976e03cb0b93909a6e05b4f5d
Marco <65359007+mghezzi-dev@users.noreply.github.com>
2021-12-13 11:04:42 +0100

    Smooth circular movement

--- Assets/Scripts/AIMovement.cs
+++ Assets/Scripts/AIMovement.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class AIMovement : MonoBehaviour
-{
-    private float timeCounter = 0;
+public class AIMovement : MonoBehaviour {
 
     private float circleRadius = 0;
     private Vector3 circlePivot = new Vector3(0f, 0f, 0f);
+
+    private int currentDirection = 1;
 
     private Rigidbody rb;
 
@@ -18,19 +18,26 @@ public class AIMovement : MonoBehaviour
     [SerializeField] public float speed = 2f;
     public float stopAt = 0.01f;
 
+
     private void Start() {
         rb = GetComponent<Rigidbody>();
 
         GetNextPivot();
     }
 
+
     private void Update() {
         if (minRadius > maxRadius)
             minRadius = maxRadius;
+
+        if (Input.GetKeyUp("up"))
+            GetNextPivot();
+
     }
 
+
     private void FixedUpdate() {
-        Quaternion q = Quaternion.AngleAxis(speed, Vector3.up);
+        Quaternion q = Quaternion.AngleAxis(speed * currentDirection, Vector3.up);
         Vector3 destination = q * (rb.transform.position - circlePivot) + circlePivot;
         rb.MovePosition(destination);
         rb.MoveRotation(rb.transform.rotation * q);
@@ -42,18 +49,28 @@ public class AIMovement : MonoBehaviour
     {
         UnityEditor.Handles.color = Color.green;
         UnityEditor.Handles.DrawWireDisc(circlePivot, Vector3.up, circleRadius);
+        UnityEditor.Handles.DrawWireDisc(circlePivot, Vector3.up, 0.1f);
     }
 
 
     private void GetNextPivot() {
         circleRadius = Random.Range(minRadius, maxRadius);
-        float zPos = 0f;
-        int dir = Random.Range(1, 3);
-        if (dir == 1)
-            zPos = circleRadius;
-        else
-            zPos = -circleRadius;
 
-        circlePivot = new Vector3(transform.position.x, transform.position.y, transform.position.z + zPos);
+        int dir = Random.Range(1, 3);
+
+        float zPos = 0f;
+        if (dir == 1) {
+            zPos = circleRadius;
+            currentDirection = 1;
+        }
+        else {
+            zPos = -circleRadius;
+            currentDirection = -1;
+        }
+
+        float x = transform.position.x + ((transform.right.x * circleRadius) * currentDirection);
+        float y = transform.position.y;
+        float z = transform.position.z + ((transform.right.z * circleRadius) * currentDirection);
+        circlePivot = new Vector3(x, y, z);
     }
 }
